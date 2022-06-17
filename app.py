5e5bac3e14270890ed3b505dade4cf4b218cedd6
xdtinker <93765440+xdtinker@users.noreply.github.com>
2022-06-17 16:13:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -62,11 +62,11 @@ async def main():
                 
                 await page.waitFor(1000)
                 if(await page.xpath('//a[contains(text(), "Let\'s review again!")]')):
-                    print(f'> Module {moduleNum} Status: X FAILED',end='\r')
+                    #print(f'> Module {moduleNum} Status: X FAILED',end='\r')
                     # await page.click(f'a[href="{moduleNum}"]')
                     moduleNum-=1
                 else: 
-                    print(f'> Module {moduleNum} Status: ✓ PASSED')
+                    #print(f'> Module {moduleNum} Status: ✓ PASSED')
                     notification(f'█ Module {moduleNum} Status: ✓ PASSED')
             except Exception as e:
                 moduleNum-=1
@@ -82,8 +82,8 @@ async def main():
             else:
                break 
         notification(f'Name: {str(_Name).upper()}\nCertID: {_CertID}')
-        print('Name:',str(_Name).upper())
-        print('CertID:',_CertID)
+        #print('Name:',str(_Name).upper())
+        #print('CertID:',_CertID)
         await browser.close()
     except Exception as e:
         notification('Something went wrong. Use /retry to try again.')
